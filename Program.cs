1b98ce8b5572d1b21da31ebe3987ca75a2ccfab5
stahl <thorsten@gmail.com>
2025-05-06 22:13:01 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text; // Bitte nicht löschen!
+
+namespace NoYelling;
+
+public class Program {
+    public static void Main(string[] args) {
+    string a = Console.ReadLine();
+    Console.WriteLine(NoYelling(a));
+    }
+
+    public static string NoYelling(string a){
+        while (a[a.Length-2]=='!'||a[a.Length-2]=='?') {
+            a = a.Substring(0, a.Length-1);
+        }
+        return a;
+    }
+}
